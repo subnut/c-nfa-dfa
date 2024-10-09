39dbb2167891be992094fcaad85b5776cbaea5db
Subhaditya Nath <sn03.general@gmail.com>
2024-10-10 00:54:37 +0530

    [WIP] nfa2dfa

--- nfa2dfa.c
+++ nfa2dfa.c
@@ -1,4 +1,5 @@
 #include "common.h"
+#include "macros.h"
 #include "dfa.h"
 #include "nfa.h"
 
@@ -71,4 +72,95 @@ move(NFA *nfa, bool *states, int input)
 		if (buf[i])
 			for (Nstatenum j = 0; j < nfa->states[i]->tnum[input]; j++)
 				states[nfa->states[i]->trans[input][j]] = true;
+}
+
+DFA *
+nfa2dfa(NFA *nfa)
+#define HASHLEN (nfa->statecount / CHAR_BIT + 1)
+{
+	NFA N = *nfa;
+
+	hash_t *hashbuf = calloc(HASHLEN, sizeof(hash_t));
+	perrif(hashbuf == NULL, "Could not allocate memory");
+
+	bool *statebuf = calloc(N.statecount, sizeof(bool));
+	perrif(statebuf == NULL, "Could not allocate memory");
+
+	/* Initialize dstates */
+	struct Dstate {
+		bool marked;
+		hash_t *hash;
+		struct Dstate *trans[ALSIZ];
+	};
+	size_t dscount = 0;
+	struct Dstate *dstates = NULL;
+
+#define dsc (dscount - 1)
+
+	/* First state (unmarked) */
+	reallocarr(dstates, ++dscount);		// allocate
+	dstates[dsc] = (struct Dstate){0};	// initialize
+	statebuf[N.start] = true;
+	setEclosure(nfa, statebuf);
+	hash(statebuf, hashbuf, N.statecount);
+	reallocarr(dstates[dsc].hash, HASHLEN);
+	memcpy(dstates[dsc].hash, hashbuf, HASHLEN);
+	dstates[dsc].marked = false;
+
+	// reset buffers
+	memset(hashbuf, 0, HASHLEN);
+	memset(statebuf, false, N.statecount);
+
+#define bufreset() {\
+	memset(hashbuf, 0, HASHLEN);\
+	memset(statebuf, false, N.statecount);}
+
+	/* main loop */
+	bool unmarked = true;
+	while (unmarked) {
+		unmarked = false;
+		for (size_t i = 0; i < dscount; i++)
+			if (!dstates[i].marked) {
+				unmarked = dstates[i].marked = true;
+				for (size_t c = 0; c < ALSIZ; c++)
+				{
+					if (!N.alphabet[c])
+						continue;
+
+					bufreset();
+					unhash(dstates[i].hash, statebuf, N.statecount);
+					move(nfa, statebuf, c);
+					setEclosure(nfa, statebuf);
+					hash(statebuf, hashbuf, N.statecount);
+
+					/* check if this state is in Dstates */
+					bool exists = false;
+					for (size_t ii = 0; ii < dscount; ii++)
+						if (0 == memcmp(dstates[ii].hash, hashbuf, HASHLEN)) {
+							exists = true;
+							break;
+						}
+					if (!exists) {
+						reallocarr(dstates, ++dscount);		// allocate
+						dstates[dsc] = (struct Dstate){0};	// initialize
+						reallocarr(dstates[dsc].hash, HASHLEN);
+						memcpy(dstates[dsc].hash, hashbuf, HASHLEN);
+						dstates[dsc].marked = false;
+					}
+					dstates[i].trans[c] = &dstates[dsc];
+				}
+			}
+	}
+
+
+	// free dstates
+	for (size_t i = 0; i < dscount; i++)
+		free(dstates[i].hash);
+	free(dstates);
+
+	// free buffers
+	free(statebuf);
+	free(hashbuf);
+
+	return NULL;
 }
