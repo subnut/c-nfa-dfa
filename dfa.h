c4c5fe0ba17e771de440266cf3fe935ae16b537c
Subhaditya Nath <sn03.general@gmail.com>
2024-10-12 12:03:46 +0530

    Add (rewritten) files dfa.c and dfa.h

---
+++ dfa.h
@@ -0,0 +1,29 @@
+#ifndef DFA_H
+#define DFA_H
+
+#include <stdbool.h>
+#include "common.h"
+
+/*
+ * Currently we are assuming each DFA only has at most UINT_MAX number of states.
+ * If the number of states were to increase beyond that, then change this typedef.
+ *
+ * NOTE: This type should always be unsigned, because signed overflow is UB.
+ */
+typedef unsigned int Dstatenum;
+
+typedef struct {
+	bool isendstate; // is endstate
+	Dstatenum dtrans[ALSIZ];
+} Dstate;
+
+typedef struct {
+	Dstatenum arrsiz;
+	Dstate *states;
+} DFA;
+
+DFA *newDFA(Dstatenum statenum);
+//Dstate *addDstate(DFA *dfa);
+void freeDFA(DFA *dfa);
+
+#endif//DFA_H
